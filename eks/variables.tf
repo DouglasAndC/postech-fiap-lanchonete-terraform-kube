a3a8875ecb8b5fb77e18fbd6c08cdfe3aa290e18
DougAndC <sousaandradedouglas@gmail.com>
2024-03-16 19:16:52 -0300

    cluster criado

---
+++ eks/variables.tf
@@ -0,0 +1,11 @@
+#variable "aws_region" {
+#  description = "AWS region"
+#}
+#
+#variable "cluster_name" {
+#  description = "Name of the EKS cluster"
+#}
+#
+#variable "cluster_version" {
+#  description = "Version of the EKS cluster"
+#}

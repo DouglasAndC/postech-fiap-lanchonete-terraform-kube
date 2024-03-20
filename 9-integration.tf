1d3461da1fa565c499873ddb931bcb5b9511f438
DougAndC <sousaandradedouglas@gmail.com>
2024-03-19 23:15:29 -0300

    format

--- 9-integration.tf
+++ 9-integration.tf
@@ -3,10 +3,10 @@ resource "aws_security_group" "vpc_link" {
   vpc_id = aws_vpc.main.id
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
